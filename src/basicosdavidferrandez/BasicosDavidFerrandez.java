fbc6897fd04d202a4026d5d34238b34c1a890b94
Leto Atreides <Vladimir Harkonnen@VladimirHarkonnen>
2018-10-16 20:05:03 +0200

    Proyecto Creado

---
+++ src/basicosdavidferrandez/BasicosDavidFerrandez.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basicosdavidferrandez;
+
+/**
+ *
+ * @author Vladimir Harkonnen
+ */
+public class BasicosDavidFerrandez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        //Variables
+        double tipoDouble;
+        int tipoEntero;
+        byte tipoByte;
+        short tipoShort;
+        
+        //Declaracion
+        tipoDouble = Double.MAX_VALUE;
+        tipoEntero = 666;
+        tipoByte = Byte.MAX_VALUE;
+        tipoShort = Short.MIN_VALUE;
+    }
+}
